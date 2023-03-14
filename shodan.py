e032c16aecff42bb511c3e09f4f739ebcd4c6649
Subinacls <admin@hacker-island.com>
2023-03-14 01:42:55 -0400

    Update shodan.py
    added some fault and error handling -

--- shodan.py
+++ shodan.py
@@ -3,6 +3,9 @@ import random
 import time
 import json
 
+# enable diagnostics
+DIAG=False
+
 # Shodan API key
 SHODAN_API_KEY = ""
 
@@ -16,44 +19,84 @@ shodan_api_url = "https://api.shodan.io/shodan/host/search?key={}&query={}".form
 # Set up a counter to keep track of the number of results returned
 result_count = 0
 
-# Loop until we have returned two results
-while True:
+def init_query(shodan_api_url, query):
     print("Searching for hosts with keyword '{}'...".format(query))
     # Send a search request to the Shodan API
-    response = requests.get(shodan_api_url)
-    if response.status_code == 200:
-        # Parse the response JSON to get the list of hosts
-        response_content = response.content.decode("utf-8")
-        response_json = json.loads(response_content)
-        hosts = response_json.get("matches")
-        if hosts:
-            # Pick a random host from the list of hosts
-            host = random.choice(hosts)
-            # Get the IP address of the host
-            ip_address = host["ip_str"]
-            # Get the port of the host
-            port = host["port"]
-            # Get the country and city of the host
-            country_name = host.get("location", {}).get("country_name", "Unknown country")
-            city_name = host.get("location", {}).get("city", "Unknown city")
-            # Get the image URL from the host
-            image_url = "https://www.shodan.io/host/{}/image".format(ip_address)
-            # Format the message for Discord
-            message = "Here's a Shodan image of {}:{} in {}, {}: {}".format(ip_address, port, city_name, country_name, image_url)
-            print("Sending message to Discord: {}".format(message))
-            # Send the message to the Discord webhook
-            response = requests.post(DISCORD_WEBHOOK_URL, json={"content": message})
-            if response.status_code == 204:
+    try:
+        response = requests.get(shodan_api_url, timeout=1000)
+    except Exception as e: # catch exceptions
+        if DIAG: print(e) # enabled diagnostics
+        time.sleep(60) # sleep, failsafe initiated
+        init_query(shodan_api_url, query) # start from initial query
+    if response.status_code == 200: # continue if 200 on GET
+        # returns JSON content from request, calls next function
+        try:
+            parse_content(json.loads(response.content))
+        except Exception as e: # catch exceptions
+            if DIAG: print(e) # enabled diagnostics
+            time.sleep(60) # sleep, failsafe initiated
+            init_query(shodan_api_url, query) # start from initial query
+    else:
+        try:
+            init_query(shodan_api_url, query) # start from initial query
+        except Exception as e: # catch exceptions
+            if DIAG: print(e) # enabled diagnostics
+            time.sleep(60) # sleep, failsafe initiated
+            init_query(shodan_api_url, query) # start from initial query
+
+def parse_content(response_data):
+    hosts = response_data.get("matches")
+    if hosts:
+        host = random.choice(hosts) # Pick a random host from the list of hosts
+        ip_address = host["ip_str"] # Get the IP address of the host
+        port = host["port"] # Get the port of the host
+        # Get the country and city of the host
+        country_name = host.get("location", {}).get("country_name", "Unknown country")
+        city_name = host.get("location", {}).get("city", "Unknown city")
+        # Get the image URL from the host
+        image_url = "https://www.shodan.io/host/{}/image".format(ip_address)
+        # Format the message for Discord
+        message = "Here's a Shodan image of {}:{} in {}, {}: {}".format(ip_address, port, city_name, country_name, image_url)
+        try:
+            discord_sender(message) # send msg to discord
+        except Exception as e: # catch exceptions
+            if DIAG: print(e) # enabled diagnostics
+            time.sleep(60) # sleep, failsafe initiated
+            try:
+                discord_sender(message) # send msg to discord
+            except Exception as e: # catch exceptions
+                if DIAG: print(e) # enabled diagnostics
+                time.sleep(60) # sleep, failsafe initiated
+                init_query(shodan_api_url, query) # start from initial query
+    else:
+        init_query(shodan_api_url, query) # start from initial query
+
+def discord_sender(message):
+        print("Sending message to Discord: {}".format(message))
+        # Send the message to the Discord webhook
+        try:
+            response = requests.post(DISCORD_WEBHOOK_URL, json={"content": message}, timeout=1000)
+            if response.status_code == 204: # continue if 204 on POST
                 # Increment the result count if the message was sent successfully
-                result_count += 1
+                result_count += 1 # increment counter for each iteration processed
                 print("Message sent successfully. Total results returned: {}".format(result_count))
                 # Wait for an hour before searching for the next image
-                time.sleep(60*60)
-        else:
-            # Wait for a few seconds if no hosts were found
-            print("No hosts found with keyword '{}'. Waiting...".format(query))
-            time.sleep(5)
-    else:
-        # Wait for a few seconds if there was an error with the Shodan API
-        print("Error searching for hosts with keyword '{}'. Status code: {}. Waiting...".format(query, response.status_code))
-        time.sleep(5)
+                time.sleep(60 * 60 * 8) # Sleep for 8 hours
+                init_query(shodan_api_url, query) # start from initial query
+            else:
+                # Wait for a few seconds if no hosts were found
+                print("Message was UNSUCCESSFUL'{}'. Waiting...".format(timeout))
+                time.sleep(60) # sleep, failsafe initiated
+                discord_sender(message) # send msg to discord
+        except Exception as e: # catch exceptions
+            if DIAG: print(e) # enabled diagnostics
+            time.sleep(60) # sleep, failsafe initiated
+            try:
+                discord_sender(message) # send msg to discord
+            except Exception as e: # catch exceptions
+                if DIAG: print(e) # enabled diagnostics
+                time.sleep(60) # sleep, failsafe initiated
+                init_query(shodan_api_url, query) # start from initial query
+
+while True:
+    init_query(shodan_api_url, query)
